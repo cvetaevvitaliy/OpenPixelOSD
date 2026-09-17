399b57f9609a5955e886a8b1dbc6abde00563d1b
Dominic Clifton <me@dominicclifton.name>
2026-09-17 10:04:31 +0200

    Changes to support VTX-Tool and VTX calibration (#34)

---
+++ src/targets/GENERIC_VTX_PA_V2/target.cmake
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# GENERIC_VTX_PA_V2 build flags -- see CMakeLists.txt "Board target selection".
+# RTC6705 VTX + Skyworks SE5004L-class linear external PA, on the optimal
+# reference pinout. TARGET_USE_PA implies VTX.
+set(TARGET_USE_PA TRUE)
